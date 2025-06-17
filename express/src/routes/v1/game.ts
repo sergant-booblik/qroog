24678a4aa4a5d0e06de6ec0a6f37278d537a3fed
sergant-booblik <serj.solntsev@gmail.com>
2025-06-17 23:52:29 +0400

    EXPRESS. UpdateGame logic created

--- express/src/routes/v1/game.ts
+++ express/src/routes/v1/game.ts
@@ -4,6 +4,7 @@ import { fetchAllPublicGames } from '@/controller/game/fetch-all-public-games';
 import { fetchAllMyGames } from '@/controller/game/fetch-all-my-games';
 import { fetchFavoritesGames } from '@/controller/game/fetch-favorites-games';
 import { fetchGame } from '@/controller/game/fetch-game';
+import { updateGame } from '@/controller/game/update-game';
 
 const gameRoutes = Router({ mergeParams: true });
 
@@ -12,7 +13,7 @@ gameRoutes.post('/', addGame);
 gameRoutes.get('/my', fetchAllMyGames);
 gameRoutes.get('/favorites', fetchFavoritesGames);
 gameRoutes.get('/:id', fetchGame);
-// gameRoutes.patch('/:id', updateGame);
+gameRoutes.patch('/:id', updateGame);
 // gameRoutes.delete('/:id', deleteGame);
 
 export default gameRoutes;
